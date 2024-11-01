dea035281c31ebed585a27411fd10cddd924cbde
sarda qayum <sardarqayu321@gmail.com>
2024-11-01 20:48:08 +0500

    change in about file

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,9 @@
   "use client"
 import React from "react"
 import Link from "next/link"
-import { useRouter } from "next/navigation"
 
 const page  =()=>{
-    const router = useRouter();
+    
     return(
         <div>
             <h1>
@@ -12,7 +11,7 @@ const page  =()=>{
                 <br />
                 <Link href="/">go to home page</Link>
                 <br />
-                <button onClick={() =>router.push("/")}>Go to login home page button</button>
+                
             </h1>
         </div>
     )
