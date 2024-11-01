fdc82c035cccb56e2cd2d786e9f3d248d7207f60
sarda qayum <sardarqayu321@gmail.com>
2024-11-01 20:41:24 +0500

    change in about file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,17 +2,16 @@
 
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 const Funtion =() =>{
-    const router = useRouter();
+   
     return(
         <div>
             <h1> Aboat page. </h1>
                 <br />
                 <Link href="/">go to home page</Link>
                 <br />
-                <button onClick={() =>router.push("/")}>Go to login home page button</button>
+                
                 <br />
                 <Link href="/about/aboutcollege/aboutschool">about college page</Link>
             
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@
 
 import React from "react"
 import Link from "next/link"
-import { useRouter } from "next/navigation"
 
 export default function Home() {
-  const router = useRouter();
+ 
   return (
     <div>
       <h1>
@@ -16,9 +15,7 @@ export default function Home() {
         <Link href="/about">go to About</Link>
         <br />
 
-        <button className="bg-red-500 hover:bg-blue-700 rounded-2xl text-white py-2 px-4 font-sans" onClick={() =>router.push("/login")}>Go to login page button</button>
-        <br /> 
-        <button className="bg-blue-500 hover:bg-red-400 rounded-2xl   text-white py-2 px-4 font-serif" onClick={() =>router.push("/about")}>Go to login page About</button>
+       
     </div>
   )
   }
